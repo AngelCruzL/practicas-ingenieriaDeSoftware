d7410d200245a7f62ddd2ef2ec957e1faa8f731c
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-02-12 22:33:30 -0600

    feat: adding signature

--- Program.cs
+++ Program.cs
@@ -10,6 +10,12 @@ namespace CAcruzlara
     {
         static void Main(string[] args)
         {
+
+
+            System.Diagnostics.Process.Start("WFcruzlara_signature.exe");
+
+            // Makes the program wait for a key press and it prevents the screen until a key is pressed.
+            Console.ReadKey();
         }
     }
 }
